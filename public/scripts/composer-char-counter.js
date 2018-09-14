f2a7b66fa28aad85f69f3c58cb5675dd76838bb2
Zoe Gelech <archaic.ghost@gmail.com>
2018-09-14 00:44:41 +0000

    working character count, start of hummingabout output box

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+  let textarea = $('textarea');
+  let counter = textarea.siblings('.counter');
+
+  textarea.on('input', function(){
+    let textarea = $(this)
+    let charIn = textarea.val().length;
+    let charCount = 140 - charIn;
+
+    counter.text(charCount)
+
+    if (charCount <= 0) {
+      counter.css('color', 'red');
+    } else { 
+      counter.css('color', 'black');
+    }
+
+  })
+});
