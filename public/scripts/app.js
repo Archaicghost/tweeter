c1103c3ac17f0400eb01d7def271619cf8f3b158
Zoe Gelech <archaic.ghost@gmail.com>
2018-09-18 17:00:37 +0000

    Have server running bugs to fix for complete load of tweeter

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,100 +3,95 @@ $(function () {
   $('#error1').hide();
   $('#error2').hide();
 
-  //Toggle compose tweet with button
+//Toggle compose tweet with button
     $( ".compose" ).click(function() {
       $( ".new-tweet" ).slideToggle( "slow" );
-      textarea.focus()
+        textarea.focus()
     });
  
-    //make new tweet with ajax and compose tweet input
-    $('#tweetForm').on('submit', function(event) {
-      event.preventDefault();
+  //make new tweet with ajax and compose tweet input
+  $('#tweetForm').on('submit', function(event) {
+    event.preventDefault();
   
-      // 1. Grab the content of the form
-      let formData = $('#tweetForm').serialize();
-      let entry = $('#textarea').val();
+  // Grab the content of the tweet textarea
+  let formData = $('#tweetForm').serialize();
+  let entry = $('#textarea').val();
       
-      console.log("I am here", entry)
-      if (entry === null || entry === ''){
+    if (entry === null || entry === ''){
         
-        // ('.error').text('Please type a tweet!')
-        $('#error1').show('fast');
-        // $('#error1').css({'opacity': '1'});
-      }else if (entry.length > 140){
-  
-        // $('#error2').css({'opacity': '1'});
-        $('#error2').show('fast');
+      $('#error1').show('fast');
+      $('#error2').hide();
         
-        // ('.error').text('Tweet has to many characters!')
-      } else {
-      // 2. Submit using ajax
+    }else if (entry.length > 140){
+        
+      $('#error2').show('fast');
+      $('#error1').hide();
+       
+    } else {
+    //Submit using ajax
       $.ajax('/tweets', {
-        method: 'POST',
-        data: formData,
+      method: 'POST',
+      data: formData,
       }).then(function() {
-        // 3. Clear the form
-        $('.textarea').val('');
-        $('.textarea').empty();
-          console.log('succesful')
-        // 4. Make sure the new tweet shows
+     //Clear the form
+      $('.counter').text(140)
+      $('#textarea').val('');
+      $('#textarea').empty();
+      $('#error1').hide();
+      $('#error2').hide();
+
+    //Make sure the new tweet shows
         return $.ajax('/tweets');
       }).then(renderTweets);
-      }
-    })
+    }
+  })
   
   //Loading new tweets and rendering previous tweets to page
   function loadTweets(){
   let $submit = $('.new-tweet-text');
-  
-    $.ajax('/tweets', { method: 'GET' })
-       .then(function (myJson) {
-         renderTweets(myJson)
-  
-      console.log('Success: ', myJson);
-  
-      $submit.replaceWith(myJson);
-      renderTweets()
-    
+ 
+  $.ajax('/tweets', { method: 'GET' })
+    .then(function (myJson) {
+      renderTweets(myJson)
+        $submit.replaceWith(myJson);
     });
   }
   loadTweets()
   
-    //rendering tweets submitted
-    function renderTweets(tweets) {
+  //rendering tweets submitted
+  function renderTweets(tweets) {
   
-      for (let userData in tweets) {
+    for (let userData in tweets) {
   
-      let tweet = tweets[userData];
-      let newComment = createTweetElement(tweet)
-       
+    let tweet = tweets[userData];
+    let newComment = createTweetElement(tweet)
+  //adding tweet to top of list
       $('.tweets-container').prepend(newComment)
-      }
     }
+  }
+  renderTweets()
   
-    //creating composed tweets with user info
-    function createTweetElement(tweet) {
+  //creating composed tweets with user info and textarea input
+  function createTweetElement(tweet) {
   
-      let $tweet = $('<article>').addClass('tweet');
-      let $commentHeader = $('<header>').addClass('commentHeader');
-      let $avatar = $('<img>').addClass('avatar').attr("src", tweet.user.avatars.small);
-      let $name = $('<name>').addClass('name').text(tweet.user.name); 
-      let $userId = $('<userId>').addClass('userId').text(tweet.user.handle);
-      let $tweetText= $('<div>').addClass('tweetText').text(tweet.content.text);
-      let $timeStamp= $('<span>').addClass('timeStamp').text(tweet.created_at);
-      let $commentFooter = $('<footer>').addClass('commentFooter');
-      let $hiddenFlag = $('<img>').addClass('hiddenIcon').attr("src", "/images/flag.png");
-      let $hiddenHeart = $('<img>').addClass('hiddenIcon').attr("src", "/images/heart.png");
-      let $hiddenReblog = $('<img>').addClass('hiddenIcon').attr("src", "/images/reblog.png");
-  
+    let $tweet = $('<article>').addClass('tweet');
+    let $commentHeader = $('<header>').addClass('commentHeader');
+    let $avatar = $('<img>').addClass('avatar').attr("src", tweet.user.avatars.small);
+    let $name = $('<name>').addClass('name').text(tweet.user.name); 
+    let $userId = $('<userId>').addClass('userId').text(tweet.user.handle);
+    let $tweetText= $('<div>').addClass('tweetText').text(tweet.content.text);
+    let $timeStamp= $('<span>').addClass('timeStamp').text(tweet.created_at);
+    let $commentFooter = $('<footer>').addClass('commentFooter');
+    let $hiddenFlag = $('<img>').addClass('hiddenIcon').attr("src", "/images/flag.png");
+    let $hiddenHeart = $('<img>').addClass('hiddenIcon').attr("src", "/images/heart.png");
+    let $hiddenReblog = $('<img>').addClass('hiddenIcon').attr("src", "/images/reblog.png");
+      //appending to document
       $commentHeader.append($avatar, $name, $userId)
-  
       $commentFooter.append($timeStamp, $hiddenReblog, $hiddenHeart, $hiddenFlag )
-      
       $tweet.append($commentHeader, $tweetText, $commentFooter)
   
-      return $tweet;
-    }
+        return $tweet;
+  }
   
   
 
--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -2,7 +2,7 @@
 
 // Simulates the kind of delay we see with network or filesystem operations
 const simulateDelay = require("./util/simulate-delay");
-
+let tweets = collection("tweets")
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
